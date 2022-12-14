d66aab7badf032f19f3e457f2e1b26144996cdb2
Nikita Kashmanov <iipabauapyka@gmail.com>
2022-12-14 12:15:09 +0500

    the fifth step in fix

---
+++ brain_games/even.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome_user
+from random import randint
+import prompt
+
+
+def even_func():
+    name = welcome_user()  # приветствие и я так привязал переменную name
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    for _ in range(3):
+        random_num = randint(1, 30)
+        if random_num % 2 == 0:
+            true_answer = 'yes'
+        else:
+            true_answer = 'no'
+        print('Question:', random_num)
+        answer = prompt.string('Your answer: ')
+        if true_answer == answer:
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{true_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+    print(f'Congratulations, {name}!')
