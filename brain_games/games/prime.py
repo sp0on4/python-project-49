d221e479011a5b77bceb4ffefe9dade6a9cbeea9
Nikita Kashmanov <iipabauapyka@gmail.com>
2022-12-15 06:16:26 +0500

    fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -10,7 +10,7 @@ def prime_func():
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
 
     for _ in range(3):
-        num = randint(1, 20)
+        num = randint(2, 20)
         true_answer = 'yes'
         for del_num in range(2, num//2+1):
             if num % del_num == 0:
